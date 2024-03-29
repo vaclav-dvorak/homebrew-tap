12d9f52165e0dcbbe43d6f417f738aa08791ff78
Vaclav Dvorak <vaclav.dvorak@bootiq.io>
2024-03-29 13:35:03 +0100

    feat: bump shas on darwin

--- Formula/veribi-cli.rb
+++ Formula/veribi-cli.rb
@@ -9,8 +9,8 @@ class VeribiCli < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/vaclav-dvorak/veribi-cli/releases/download/v0.1.11/veribi_darwin_arm64.tar.gz"
-      sha256 "8662944da45119f2b57c1bc14821c1a55b84401dc9f3020006dac4370a3f46b8"
+      url "https://github.com/vaclav-dvorak/veribi-cli/releases/download/v0.1.12/veribi_darwin_arm64.tar.gz"
+      sha256 "ce01717978330bdfa5ee0a18d0aad72a364492bd776225aea0c84f6134704b0a"
 
       def install
         bin.install "veribi"
@@ -20,8 +20,8 @@ class VeribiCli < Formula
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/vaclav-dvorak/veribi-cli/releases/download/v0.1.11/veribi_darwin_amd64.tar.gz"
-      sha256 "91f8b81dc9358397e00ca22a1838308f7bef80155ebed8837aa01b9fee134edf"
+      url "https://github.com/vaclav-dvorak/veribi-cli/releases/download/v0.1.12/veribi_darwin_amd64.tar.gz"
+      sha256 "8981e4b1c8b692b9be8bd18cbaf99dcb0cb6d1af92dd9644779a6fe6ae0f9938"
 
       def install
         bin.install "veribi"
@@ -45,7 +45,7 @@ class VeribiCli < Formula
       end
     end
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/vaclav-dvorak/veribi-cli/releases/download/v0.1.11/veribi_linux_arm.tar.gz"
+      url "https://github.com/vaclav-dvorak/veribi-cli/releases/download/v0.1.12/veribi_linux_arm.tar.gz"
       sha256 "3b4a61b7c3e003ba9b1385ca9fa7fbf9f219a8065b4c1f7683928045b26fbbbb"
 
       def install
@@ -56,7 +56,7 @@ class VeribiCli < Formula
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/vaclav-dvorak/veribi-cli/releases/download/v0.1.11/veribi_linux_arm64.tar.gz"
+      url "https://github.com/vaclav-dvorak/veribi-cli/releases/download/v0.1.12/veribi_linux_arm64.tar.gz"
       sha256 "accc712d01b7ac896495696c04fe77415bfc37efbed4317a85620d510e739c88"
 
       def install
